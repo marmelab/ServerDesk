a27d9ed688c24cb440680c6649942f90a1243e5d
Jerome <jerome@marmelab.com>
2026-03-06 15:57:25 +0100

    format

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -81,7 +81,8 @@ export const LoginForm = ({
               <div className="grid gap-2">
                 <div className="flex items-center">
                   <Label htmlFor="password">Password</Label>
-                  <Link to="/auth/forgot-password"
+                  <Link
+                    to="/auth/forgot-password"
                     className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
                   >
                     Forgot your password?
