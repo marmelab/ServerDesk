faf4acce4bde9b182ae1d2a8d1cec29314131734
Jerome <jerome@marmelab.com>
2026-03-09 11:42:59 +0100

    format

--- src/pages/companies.test.tsx
+++ src/pages/companies.test.tsx
@@ -4,21 +4,25 @@ import { describe, it, expect, vi } from 'vitest';
 import CompaniesPage from './companies';
 import userEvent from '@testing-library/user-event';
 
-let mockCompanies = [{ id: 1, name: 'acme', created_at: '2026-02-25T00:00:00Z' }];
+let mockCompanies = [
+  { id: 1, name: 'acme', created_at: '2026-02-25T00:00:00Z' },
+];
 
 vi.mock('@/lib/supabase', () => ({
   supabase: {
     from: vi.fn(() => ({
-      select: vi.fn().mockImplementation(() => Promise.resolve({
-        data: mockCompanies,
-        error: null
-      })),
+      select: vi.fn().mockImplementation(() =>
+        Promise.resolve({
+          data: mockCompanies,
+          error: null,
+        }),
+      ),
       insert: vi.fn((newData) => {
         const record = Array.isArray(newData) ? newData[0] : newData;
         const newEntry = {
           id: Math.random(),
           name: record.name,
-          created_at: new Date().toISOString()
+          created_at: new Date().toISOString(),
         };
 
         mockCompanies.push(newEntry);
@@ -48,19 +52,25 @@ describe('CompaniesPage', () => {
   it('opens dialog and adds a new company to the list', async () => {
     const user = userEvent.setup();
     render(<CompaniesPage />, { wrapper: Wrapper });
-    const buttonAddCompany = screen.getByRole('button', { name: /add company/i });
+    const buttonAddCompany = screen.getByRole('button', {
+      name: /add company/i,
+    });
     await user.click(buttonAddCompany);
 
-    const inputCompanyName = await screen.findByRole('textbox', { name: /company/i });
+    const inputCompanyName = await screen.findByRole('textbox', {
+      name: /company/i,
+    });
     expect(inputCompanyName).toBeInTheDocument();
 
     await user.type(inputCompanyName, 'New company');
     expect(inputCompanyName).toHaveValue('New company');
 
-    const buttonInsertCompany = await screen.findByRole('button', { name: /add/i });
+    const buttonInsertCompany = await screen.findByRole('button', {
+      name: /add/i,
+    });
     await user.click(buttonInsertCompany);
 
     screen.debug();
     expect(await screen.findByText('New company')).toBeInTheDocument();
-  })
+  });
 });
