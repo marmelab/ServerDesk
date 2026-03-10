a84aa22bd999eaacae78251d3f00db4aa751371f
Jerome <jerome@marmelab.com>
2026-03-10 16:32:31 +0100

    Modifiy function name for handling user signup

---
+++ supabase/migrations/20260310153056_modify_function_name_handle_user_signup.sql
@@ -0,0 +1,48 @@
+drop function if exists "public"."handle_first_admin"();
+
+CREATE OR REPLACE FUNCTION public.handle_user_signup()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$declare
+  admin_count int;
+  user_name text;
+  meta_company_id int8;
+  invite_token uuid;
+  user_role public.app_role;
+begin
+  user_name := COALESCE(
+      new.raw_user_meta_data ->> 'name', 
+      'User-' || RIGHT(new.id::text, 4)
+    );
+
+  meta_company_id := (new.raw_user_meta_data ->> 'company_id')::int8;
+
+  invite_token := (new.raw_user_meta_data ->> 'invite_token')::uuid;
+
+  if meta_company_id is not null then
+    user_role := 'customer_manager';
+  else
+    select count(*) into admin_count from public.app_user where role = 'admin';
+
+    if admin_count > 0 then
+      user_role = 'agent';
+    else
+      user_role = 'admin';
+    end if;
+  end if;
+
+  if invite_token is not null then
+    update public.invite_tokens
+    set used_at = now()
+    where token = invite_token;
+  end if;
+
+  insert into public.app_user (id, name, role)
+  values (
+    new.id, 
+    user_name, 
+    user_role);
+  return new;
+end;$function$
+;
