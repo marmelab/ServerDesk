fec6ac331af2bda0e2c5dae1f82c83a075fc9b32
Jerome <jerome@marmelab.com>
2026-03-18 11:08:07 +0100

    fix PR

--- supabase/migrations/20260317094530_add_view_agent_details.sql
+++ supabase/migrations/20260317094530_add_view_agent_details.sql
@@ -1,13 +1,21 @@
-create or replace view "public"."agent_details" as  SELECT au.id,
-    u.email,
-    au.name,
-    COALESCE(json_agg(c.name) FILTER (WHERE (c.name IS NOT NULL)), '[]'::json) AS company_names
-   FROM (((public.app_user au
-     JOIN auth.users u ON ((au.id = u.id)))
-     LEFT JOIN public.user_companies uc ON ((au.id = uc.user_id)))
-     LEFT JOIN public.companies c ON ((uc.company_id = c.id)))
-  WHERE (au.role = 'agent'::public.app_role)
-  GROUP BY au.id, u.email, au.name;
-
-
-
+create or replace view "public"."agent_details" as
+select
+  au.id,
+  u.email,
+  au.name,
+  COALESCE(
+    json_agg(c.name) filter (where c.name is not null),
+    '[]'::json
+  ) as company_names
+from public.app_user au
+join auth.users u 
+  on au.id = u.id
+left join public.user_companies uc
+  on au.id = uc.user_id
+left join public.companies c 
+  on uc.company_id = c.id
+where au.role = 'agent'::public.app_role
+group by 
+  au.id, 
+  u.email, 
+  au.name;
