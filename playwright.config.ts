0698dff83afe445bfa2834abc89c998c2d22fac5
Jerome <jerome@marmelab.com>
2026-03-19 17:06:23 +0100

    test ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,5 +40,10 @@ export default defineConfig({
     reuseExistingServer: !process.env.CI,
     stdout: 'pipe', // Useful for debugging CI logs
     stderr: 'pipe',
+    env: {
+      VITE_SUPABASE_URL:
+        process.env.VITE_SUPABASE_URL || 'http://127.0.0.1:54321',
+      VITE_SUPABASE_ANON_KEY: process.env.VITE_SUPABASE_ANON_KEY || '',
+    },
   },
 });
