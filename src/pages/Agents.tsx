50a6fb2f2f21a1ecaeac182f1f10ecaa9459a554
Jerome <jerome@marmelab.com>
2026-03-23 16:54:31 +0100

    Components separation : add AgentCard

--- src/pages/Agents.tsx
+++ src/pages/Agents.tsx
@@ -1,24 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
 import { UserPlus } from 'lucide-react';
-import { Badge } from '@/components/ui/badge';
 import { useState } from 'react';
 import { InviteDialog } from '@/components/InviteDialog';
 import { fetchAgents } from '@/services/Agents';
-import { AssignCompaniesDialog } from '@/components/AssignCompaniesDialog';
-import { AgentDetails, CompanyJson } from '@/types';
+import AgentCard from '@/components/AgentCard';
 
 export default function AgentsPage() {
   const [isInviteOpen, setIsInviteOpen] = useState(false);
-  const [isAssignCompaniesOpen, setIsAssignCompaniesOpen] = useState(false);
-  const [agent, setAgent] = useState<AgentDetails>();
 
   const {
     data: agents = [],
@@ -31,11 +20,6 @@ export default function AgentsPage() {
 
   const handleOpenInvite = () => {
     setIsInviteOpen(true);
-  };
-
-  const handleAssignCompanies = (agent: AgentDetails) => {
-    setAgent(agent);
-    setIsAssignCompaniesOpen(true);
   };
 
   if (isPending)
@@ -71,55 +55,7 @@ export default function AgentsPage() {
           ) : (
             <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
               {agents.map((agent) => (
-                <Card
-                  key={agent.id}
-                  className="group relative flex flex-col gap-2 overflow-hidden pt-0"
-                  data-testid="agent-card"
-                >
-                  <CardHeader className="flex-1 pt-4">
-                    <CardTitle className="text-2xl font-semibold text-balance">
-                      {agent.name}
-                    </CardTitle>
-                    <CardDescription>{agent.email}</CardDescription>
-                  </CardHeader>
-
-                  <CardFooter className="flex flex-row flex-wrap gap-2 pt-4">
-                    {(agent.companies as CompanyJson[])?.length > 0 ? (
-                      (agent.companies as CompanyJson[]).map((company) => (
-                        <Badge
-                          key={company.name}
-                          variant="secondary"
-                          className="whitespace-nowrap"
-                        >
-                          {company.name}
-                        </Badge>
-                      ))
-                    ) : (
-                      <span className="text-sm text-muted-foreground">
-                        No companies assigned
-                      </span>
-                    )}
-                    <Button
-                      className="group-hover:bg-primary group-hover:text-primary-foreground w-full cursor-pointer"
-                      onClick={() => handleAssignCompanies(agent)}
-                    >
-                      Assign companies
-                      <svg
-                        className="ms-2 size-4"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M14 5l7 7m0 0l-7 7m7-7H3"
-                        />
-                      </svg>
-                    </Button>
-                  </CardFooter>
-                </Card>
+                <AgentCard agentInfos={agent} />
               ))}
             </div>
           )}
@@ -129,14 +65,6 @@ export default function AgentsPage() {
             initialCompanyId={null}
             appRole={'agent'}
           />
-
-          {agent && (
-            <AssignCompaniesDialog
-              open={isAssignCompaniesOpen}
-              onOpenChange={setIsAssignCompaniesOpen}
-              agent={agent}
-            />
-          )}
         </div>
       )}
     </div>
