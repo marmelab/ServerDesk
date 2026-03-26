66476e63969d0ee097da719e1a43827beb483fdc
Jerome <jerome@marmelab.com>
2026-03-26 10:28:13 +0100

    fix storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,8 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [
-    '@chromatic-com/storybook',
-    '@storybook/addon-vitest',
-    '@storybook/addon-a11y',
-    '@storybook/addon-docs',
-    '@storybook/addon-onboarding',
-  ],
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: ['@storybook/addon-vitest', '@storybook/addon-a11y'],
   framework: '@storybook/react-vite',
 };
 export default config;
