3adda88d6f519864947c9e5a6da3884ea739e62c
Jerome <jerome@marmelab.com>
2026-04-01 13:51:47 +0200

    Add e2e tests for validotrs forms

--- e2e/customers.spec.ts
+++ e2e/customers.spec.ts
@@ -15,7 +15,8 @@ test.describe('Customers Page', () => {
     await page
       .getByRole('textbox', { name: 'Email' })
       .fill('customer@test.com');
-    await page.getByRole('button', { name: 'Add Customer' }).click();
+    const addButton = page.getByRole('button', { name: 'Add Customer' });
+    await addButton.click();
 
     // Find customer role and click edit
     const row = page.getByRole('row').filter({ hasText: uniqueName });
@@ -29,6 +30,18 @@ test.describe('Customers Page', () => {
     // Find new edit name
     const editRow = page.getByRole('row').filter({ hasText: uniqueName });
     await expect(editRow).toContainText(uniqueName);
+
+    // Try to add without fields
+    await page.getByRole('button', { name: 'Add Customer' }).click();
+    await expect(addButton).toBeDisabled();
+    await page.getByRole('textbox', { name: 'Name' }).fill(uniqueName);
+    await expect(addButton).toBeDisabled();
+    await page.getByRole('textbox', { name: 'Email' }).click();
+    await page
+      .getByRole('textbox', { name: 'Email' })
+      .fill('customer@test.com');
+    await page.getByRole('button', { name: 'Add Customer' }).click();
+    await expect(page.getByText(/this record already exists/i)).toBeVisible();
 
     // Test delete customer
     await editRow.getByRole('button', { name: 'Delete customer' }).click();
